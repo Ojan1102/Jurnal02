b9511de8f1fea73281f3846e84ce629c84a5e40f
Ojan1102 <71244380+Ojan1102@users.noreply.github.com>
2020-09-18 09:10:09 +0700

    TugasPertama.java

---
+++ Praktikum2/TugasPertama.java
@@ -0,0 +1,9 @@
+public class TugasPertama {
+
+	public static void main(String[] args){
+		System.out.println("Nama: Muhammad Fauzan");
+		System.out.println("Nim: 6706204011");
+		System.out.println("Kelas: 44-03");
+	}
+}
+		
