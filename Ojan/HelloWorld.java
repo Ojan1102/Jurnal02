385176e351041ba066aed61bcddc17ce72a0626f
Muhammad Fauzan <71244380+Ojan1102@users.noreply.github.com>
2020-09-18 09:03:12 +0700

    Update HelloWorld.java

--- Ojan/HelloWorld.java
+++ Ojan/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Halo Semuanyanya!");
+		System.out.println("Nama: Muhammad Fauzan");
+		System.out.println("Nim: 6706204011");
+		System.out.println("Kelas: 44-03");
 	}
 }
