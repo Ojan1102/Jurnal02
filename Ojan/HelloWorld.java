738d4aa8278793e9f286f978b6e92302ec6ebc2d
Muhammad Fauzan <71244380+Ojan1102@users.noreply.github.com>
2020-09-18 09:05:18 +0700

    Delete HelloWorld.java

--- Ojan/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Nama: Muhammad Fauzan");
-		System.out.println("Nim: 6706204011");
-		System.out.println("Kelas: 44-03");
-	}
-}
